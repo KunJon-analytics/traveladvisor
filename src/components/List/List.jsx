79f559caceb55ea09bcaa77d7491641be71d2758
John Adelakun <johnadelakun1.com>
2022-03-01 12:47:30 +0100

    Completed placeDetails card

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -12,22 +12,10 @@ import {
 import PlaceDetails from "../PlaceDetails/PlaceDetails";
 import useStyles from "./styles.js";
 
-const List = () => {
+const List = ({ places }) => {
   const classes = useStyles();
   const [type, setType] = useState("restaurants");
   const [rating, setRating] = useState("");
-
-  const places = [
-    { name: "Cool Place" },
-    { name: "Cool Place" },
-    { name: "Cool Place" },
-    { name: "Cool Place" },
-    { name: "Cool Place" },
-    { name: "Cool Place" },
-    { name: "Cool Place" },
-    { name: "Cool Place" },
-    { name: "Cool Place" },
-  ];
 
   return (
     <div className={classes.container}>
